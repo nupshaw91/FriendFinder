7bd7ede3f794c52f5e5eabb85664899c4267f075
Nicholas Upshaw <nicholas.upsha@yahoo.com>
2020-02-05 19:07:24 -0500

    Set up server file

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
-var express = require("express")
-var path = require("path")
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var port = process.env.port || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("./public"));
+
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+app.listen(port, function(){
+    console.log("Your App is listening on PORT:" + port);
+});
